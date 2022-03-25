7218b82b8fb3b83a09360e7d5e751a5cedf289d0
Yazdun <yazduntube@gmail.com>
2022-03-25 20:48:30 +0430

    models : completed post model

--- models/Post.js
+++ models/Post.js
@@ -8,6 +8,11 @@ const PostSchema = new mongoose.Schema(
       minlength: [3, 'title must be at least 3 characters'],
       maxlength: [50, 'title must be less than 50 characters'],
     },
+    shortTitle: {
+      type: String,
+      required: [true, 'title is required'],
+      maxlength: [50, 'title must be less than 50 characters'],
+    },
     description: {
       type: String,
       required: [true, 'description is required'],
@@ -18,6 +23,17 @@ const PostSchema = new mongoose.Schema(
       required: [true, 'content is required'],
       minlength: [3, 'describtion must be at least 3 characters'],
     },
+    category: {
+      enum: ['personal', 'frontend', 'backend', 'blockchain'],
+      type: String,
+    },
+    tags: {
+      type: [String],
+    },
+    image: String,
+    twitterUrl: String,
+    codepen: String,
+    codesandbox: String,
   },
   { timestamps: true },
 )
